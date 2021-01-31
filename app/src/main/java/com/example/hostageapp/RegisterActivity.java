9497dc45f31ab1f5af046d300242a3bdcfeafe7b
Tushar Amrit <tusharpubg02@gmail.com>
2021-01-31 21:23:00 +0530

    RegisterActivity Updated

--- app/src/main/java/com/example/hostageapp/RegisterActivity.java
+++ app/src/main/java/com/example/hostageapp/RegisterActivity.java
@@ -20,7 +20,7 @@ public class RegisterActivity extends AppCompatActivity {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             Window window = getWindow();
             window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-//            window.setStatusBarColor(Color.TRANSPARENT);
+
             window.setStatusBarColor(getResources().getColor(R.color.register_bk_color));
         }
     }
