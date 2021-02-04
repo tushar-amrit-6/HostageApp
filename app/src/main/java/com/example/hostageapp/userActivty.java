b10fa07fd42697d3a8cb8a99a11f8b8cc533b711
Tushar Amrit <tusharpubg02@gmail.com>
2021-02-04 23:08:08 +0530

    navbar updated

--- app/src/main/java/com/example/hostageapp/userActivty.java
+++ app/src/main/java/com/example/hostageapp/userActivty.java
@@ -8,42 +8,47 @@ import android.os.Bundle;
 import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.ismaeldivita.chipnavigation.ChipNavigationBar;
 
 public class userActivty extends AppCompatActivity {
 
-    private BottomNavigationView bottomNavigationView;
+    private ChipNavigationBar chipNavigationBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_activty);
 
-        bottomNavigationView=findViewById(R.id.bottomNavigationView);
-        bottomNavigationView.setOnNavigationItemSelectedListener(bottomNavMethod);
+        chipNavigationBar=findViewById(R.id.bottomNavigationView);
+        chipNavigationBar.setItemSelected(R.id.bottomNavigationView,true);
         getSupportFragmentManager().beginTransaction().replace(R.id.flFragment,new userHomeFragment()).commit();
+
+        bottomMenu();
+
     }
-    private BottomNavigationView.OnNavigationItemSelectedListener bottomNavMethod=new
-            BottomNavigationView.OnNavigationItemSelectedListener() {
-                @Override
-                public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-                    Fragment fragment = null;
-                    switch (menuItem.getItemId())
-                    {
-                        case R.id.miHome:
-                            fragment= new userHomeFragment();
-                            break;
-                        case R.id.miChat:
-                            fragment= new userChatFragment();
-                            break;
-                        case R.id.miMap:
-                            fragment= new userMapFragment();
-                            break;
-                    }
-                    getSupportFragmentManager().beginTransaction().replace(R.id.flFragment,fragment).commit();
-                    return true;
+
+    private void bottomMenu() {
+        chipNavigationBar.setOnItemSelectedListener(new ChipNavigationBar.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(int i) {
+                Fragment fragment = null;
+                switch (i) {
+                    case R.id.miHome:
+                        fragment = new userHomeFragment();
+                        break;
+                    case R.id.miChat:
+                        fragment = new userChatFragment();
+                        break;
+                    case R.id.miMap:
+                        fragment = new userMapFragment();
+                        break;
+                    case R.id.miSymptoms:
+                        fragment = new userSymptoms();
+                        break;
                 }
+                getSupportFragmentManager().beginTransaction().replace(R.id.flFragment,fragment).commit();
+            }
+        });
+    }
+    }
 
-
-
-            };
-}
---
+++ app/src/main/java/com/example/hostageapp/userSymptoms.java
@@ -0,0 +1,65 @@
+package com.example.hostageapp;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class userSymptoms extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public userSymptoms() {
+        // Required empty public constructor
+    }
+
+
+    // TODO: Rename and change types and number of parameters
+    public static userSymptoms newInstance(String param1, String param2) {
+        userSymptoms fragment = new userSymptoms();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+
+
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View v= inflater.inflate(R.layout.fragment_user_symptoms,container,false);
+        WebView webView = (WebView)v.findViewById(R.id.webView);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://upgradeplan.hellotars.com/conv/ryBF5K/?convid=41BcE1");
+        return v;
+
+
+
+    }
+}
